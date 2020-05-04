87b27d470661083685dcf29bda0aba28012083e5
CC <774629027@qq.com>
2020-05-04 23:37:02 +0800

    add mock js

--- vue.config.js
+++ vue.config.js
@@ -29,7 +29,7 @@ module.exports = {
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
-  devServer: {
+    : {
     port: port,
     open: false,
     overlay: {
